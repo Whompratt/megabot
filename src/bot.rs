73aa82539850ea235ed8e57df976fe779e8040ab
Whompratt <JakeR1342@gmail.com>
2022-08-08 21:06:29 +0100

    Basic bot configuration done.
    Co-authored-by: drifio <drifio@users.noreply.github.com>

---
+++ src/bot.rs
@@ -0,0 +1,45 @@
+use poise::serenity_prelude as serenity;
+use std::env::var;
+type Error = Box<dyn std::error::Error + Send + Sync>;
+use dotenv::dotenv;
+
+pub struct Data {}
+
+async fn on_error(error: poise::FrameworkError<'_, Data, Error>) {
+    match error {
+        poise::FrameworkError::Setup { error } => panic!("Failed to start bot: {:?}", error),
+        poise::FrameworkError::Command { error, ctx } => {
+            println!("Error in command '{}': {:?}", ctx.command().name, error);
+        }
+        error => {
+            if let Err(e) = poise::builtins::on_error(error).await {
+                println!("Error while handling error: {}", e)
+            }
+        }
+    }
+}
+
+pub async fn start_bot() {
+    dotenv().ok();
+
+    let options = poise::FrameworkOptions {
+        commands: vec![],
+        prefix_options: poise::PrefixFrameworkOptions {
+            prefix: Some("!".into()),
+            // edit_tracker: Some(poise::EditTracker::for_timespan(Duration::from_secs(3600))),
+            ..Default::default()
+        },
+        on_error: |error| Box::pin(on_error(error)),
+        ..Default::default()
+    };
+
+    let framework = poise::Framework::builder()
+        .token(var("DISCORD_TOKEN").expect("token"))
+        .user_data_setup(move |_ctx, _ready, _framework| Box::pin(async move { Ok(Data {}) }))
+        .options(options)
+        .intents(
+            serenity::GatewayIntents::non_privileged() | serenity::GatewayIntents::MESSAGE_CONTENT,
+        );
+
+    framework.run().await.unwrap();
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
-fn main() {
-    println!("Hello, world!");
+mod bot;
+use crate::bot::start_bot;
+use dotenv::dotenv;
+use log::{debug, error};
+
+#[tokio::main]
+async fn main() {
+    dotenv().ok();
+    env_logger::init();
+
+    start_bot().await;
 }
